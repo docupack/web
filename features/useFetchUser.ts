470a4adb4e474cf04e5f053c87495d7834bd8000
Can Sirin <cansirin12@gmail.com>
2021-07-02 16:22:25 -0700

    useInterval and useFetchUser hooks

--- features/useFetchUser.ts
+++ features/useFetchUser.ts
@@ -1,35 +1,33 @@
-import React, { Dispatch, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Auth } from "aws-amplify";
+import { AuthUser } from "./auth/utils/user-context";
 
-export interface AuthUser {
-  username: string;
-  attributes: {
-    email: string;
-  };
-}
+export const fetchUser = async (auth: typeof Auth) => {
+  return await auth.currentAuthenticatedUser();
+};
 
 export const useFetchUser = (): [
   AuthUser,
-  Dispatch<React.SetStateAction<AuthUser>>,
   { error: Error | null; loading: boolean }
 ] => {
   const [user, setUser] = useState<AuthUser | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
+  const asyncEffect = async () => {
+    setLoading(true);
+    try {
+      const u = await fetchUser(Auth);
+      setUser(u);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  };
   useEffect(() => {
-    const asyncEffect = async () => {
-      try {
-        const u = await Auth.currentAuthenticatedUser();
-        setUser(u);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError(error);
-      }
-    };
     asyncEffect();
   }, []);
 
-  return [user, setUser, { loading, error }];
+  return [user, { loading, error }];
 };
